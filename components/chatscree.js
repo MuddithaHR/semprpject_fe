47a8860cc7cd6c567402e88f3ba1fc9df13761f3
Dilshan Rakshitha <dilshanrakshitha2000@gmail.com>
2023-08-31 10:49:41 +0530

    Update chatscree.js
    translate

--- components/chatscree.js
+++ components/chatscree.js
@@ -1,8 +1,8 @@
-import React, { useState, useEffect, useCallback } from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, StyleSheet } from 'react-native';
-import { Bubble, GiftedChat, Send } from 'react-native-gifted-chat';
-import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
+import { Bubble, GiftedChat } from 'react-native-gifted-chat';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
+const { translate } = require('@vitalets/google-translate-api');
 
 const ChatScreen = () => {
   const [messages, setMessages] = useState([]);
@@ -11,42 +11,66 @@ const ChatScreen = () => {
     setMessages([]);
   }, []);
 
-  const onSend = useCallback((messages = []) => {
-    setMessages((previousMessages) =>
-      GiftedChat.append(previousMessages, messages),
-    );
-  }, []);
+  const onSend = async (newMessages = []) => {
+    const userMessage = newMessages[0];
 
-  const sendMessageToRasa = async (message) => {
+    // Display user's message immediately
+    setMessages((previousMessages) =>
+      GiftedChat.append(previousMessages, [
+        {
+          ...userMessage,
+          _id: messages.length + 1,
+          createdAt: new Date(),
+        },
+      ])
+    );
+
+    // Translate user's message from Sinhala to English
+    const translatedMessage = await translate(userMessage.text, { to: 'en' });
+
     try {
-      console.log(message);
       const response = await fetch(
-        'https://4176-223-224-1-6.ngrok.io/webhooks/rest/webhook',
+        'https://23ad-223-224-1-6.ngrok.io/webhooks/rest/webhook',
         {
           method: 'POST',
           headers: {
-            'Accept': 'application/json',
+            Accept: 'application/json',
           },
           credentials: 'same-origin',
-          body: JSON.stringify({ message: message.text }),
+          body: JSON.stringify({ message: translatedMessage.text }),
         }
       );
 
       const data = await response.json();
       if (data && data.length > 0) {
-        const botMessages = data.map((responseData) => ({
-          _id: Math.random().toString(36).substring(7),
-          text: responseData.text,
-          createdAt: new Date(),
-          user: {
-            _id: 2,
-            name: 'Bot',
-            avatar: 'https://placeimg.com/140/140/any',
-          },
-        }));
+        const botMessages = [];
+        for (const botResponse of data) {
+          if (botResponse.text) {
+            const botMessage = {
+              _id: messages.length + botMessages.length + 2,
+              text: botResponse.text,
+              createdAt: new Date(),
+              user: {
+                _id: 2,
+                name: 'Bot',
+                avatar: 'https://placeimg.com/140/140/any',
+              },
+            };
+            botMessages.push(botMessage);
+          }
+        }
 
+        // Translate the bot's responses from English to Sinhala
+        const translatedResponses = await Promise.all(
+          botMessages.map(async (botMsg) => {
+            const translatedResponse = await translate(botMsg.text, { to: 'si' });
+            return { ...botMsg, text: translatedResponse.text };
+          })
+        );
+
+        // Display bot's responses
         setMessages((previousMessages) =>
-          GiftedChat.append(previousMessages, botMessages)
+          GiftedChat.append(previousMessages, translatedResponses)
         );
       }
     } catch (error) {
@@ -75,38 +99,19 @@ const ChatScreen = () => {
     );
   };
 
-  const renderSend = (props) => {
-    return (
-      <Send {...props}>
-        <View>
-          <MaterialCommunityIcons
-            name="send-circle"
-            style={{ marginBottom: 5, marginRight: 5 }}
-            size={32}
-            color="#1976D2"
-          />
-        </View>
-      </Send>
-    );
-  };
-
   const scrollToBottomComponent = () => {
-    return <FontAwesome name="angle-double-down" size={22} color="#333" />;
+    return <FontAwesome name='angle-double-down' size={22} color='#333' />;
   };
 
   return (
     <GiftedChat
       messages={messages}
-      onSend={(messages) => {
-        onSend(messages);
-        sendMessageToRasa(messages[0]);
-      }}
+      onSend={onSend}
       user={{
         _id: 1,
       }}
       renderBubble={renderBubble}
       alwaysShowSend
-      renderSend={renderSend}
       scrollToBottom
       scrollToBottomComponent={scrollToBottomComponent}
     />
