992bbb86bf8a6386d8ccd7d8dc28be02e3428098
Dilshan Rakshitha <dilshanrakshitha2000@gmail.com>
2023-08-31 09:56:02 +0530

    change

--- components/chatscree.js
+++ components/chatscree.js
@@ -1,6 +1,6 @@
-import React, {useState, useEffect, useCallback} from 'react';
-import {View, ScrollView, Text, Button, StyleSheet} from 'react-native';
-import {Bubble, GiftedChat, Send} from 'react-native-gifted-chat';
+import React, { useState, useEffect, useCallback } from 'react';
+import { View, StyleSheet } from 'react-native';
+import { Bubble, GiftedChat, Send } from 'react-native-gifted-chat';
 import MaterialCommunityIcons from 'react-native-vector-icons/MaterialCommunityIcons';
 import FontAwesome from 'react-native-vector-icons/FontAwesome';
 
@@ -8,28 +8,7 @@ const ChatScreen = () => {
   const [messages, setMessages] = useState([]);
 
   useEffect(() => {
-    setMessages([
-      {
-        _id: 1,
-        text: 'Hello develper',
-        createdAt: new Date(),
-        user: {
-          _id: 2,
-          name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any',
-        },
-      },
-      {
-        _id: 2,
-        text: 'Hello world',
-        createdAt: new Date(),
-        user: {
-          _id: 1,
-          name: 'React Native',
-          avatar: 'https://placeimg.com/140/140/any',
-        },
-      },
-    ]);
+    setMessages([]);
   }, []);
 
   const onSend = useCallback((messages = []) => {
@@ -38,19 +17,41 @@ const ChatScreen = () => {
     );
   }, []);
 
-  const renderSend = (props) => {
-    return (
-      <Send {...props}>
-        <View>
-          <MaterialCommunityIcons
-            name="send-circle"
-            style={{marginBottom: 5, marginRight: 5}}
-            size={32}
-            color="#1976D2"
-          />
-        </View>
-      </Send>
-    );
+  const sendMessageToRasa = async (message) => {
+    try {
+      console.log(message);
+      const response = await fetch(
+        'https://4176-223-224-1-6.ngrok.io/webhooks/rest/webhook',
+        {
+          method: 'POST',
+          headers: {
+            'Accept': 'application/json',
+          },
+          credentials: 'same-origin',
+          body: JSON.stringify({ message: message.text }),
+        }
+      );
+
+      const data = await response.json();
+      if (data && data.length > 0) {
+        const botMessages = data.map((responseData) => ({
+          _id: Math.random().toString(36).substring(7),
+          text: responseData.text,
+          createdAt: new Date(),
+          user: {
+            _id: 2,
+            name: 'Bot',
+            avatar: 'https://placeimg.com/140/140/any',
+          },
+        }));
+
+        setMessages((previousMessages) =>
+          GiftedChat.append(previousMessages, botMessages)
+        );
+      }
+    } catch (error) {
+      console.error('Error communicating with Rasa:', error);
+    }
   };
 
   const renderBubble = (props) => {
@@ -74,17 +75,32 @@ const ChatScreen = () => {
     );
   };
 
-  const scrollToBottomComponent = () => {
-    return(
-      <FontAwesome name='angle-double-down' size={22} color='#333' />
+  const renderSend = (props) => {
+    return (
+      <Send {...props}>
+        <View>
+          <MaterialCommunityIcons
+            name="send-circle"
+            style={{ marginBottom: 5, marginRight: 5 }}
+            size={32}
+            color="#1976D2"
+          />
+        </View>
+      </Send>
     );
-  }
+  };
+
+  const scrollToBottomComponent = () => {
+    return <FontAwesome name="angle-double-down" size={22} color="#333" />;
+  };
 
   return (
     <GiftedChat
       messages={messages}
-      onSend={(messages) => onSend(messages)}
-      style={styles.messages}
+      onSend={(messages) => {
+        onSend(messages);
+        sendMessageToRasa(messages[0]);
+      }}
       user={{
         _id: 1,
       }}
@@ -104,6 +120,5 @@ const styles = StyleSheet.create({
     flex: 1,
     alignItems: 'center',
     justifyContent: 'center',
-  }
+  },
 });
-
